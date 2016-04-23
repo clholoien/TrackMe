dbe3db1d84a7bbf3791c5268a0586e2e81dbe1e5
laurendumapias <laurendumapias@gmail.com>
2016-04-23 00:59:01 -0700

    Worked on JSON

---
+++ TrackMe/TrackMe/RestAPIManager.swift
@@ -0,0 +1,42 @@
+//
+//  RestAPIManager.swift
+//  TrackMe
+//
+//  Created by Lauren Dumapias on 4/23/16.
+//  Copyright © 2016 Carson Holoien/ Lauren Dumapias. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+typealias ServiceResponse = (JSON, NSError?) -> Void
+
+class RestApiManager: NSObject {
+    static let sharedInstance = RestApiManager()
+    
+    let baseURL = "https://sandbox.shiphawk.com"
+    
+    func getRandomUser(onCompletion: (JSON) -> Void) {
+        let route = baseURL
+        makeHTTPGetRequest(route, onCompletion: { json, err in
+            onCompletion(json as JSON)
+        })
+    }
+    
+    // MARK: Perform a GET Request
+    private func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            if let jsonData = data {
+                let json:JSON = JSON(data: jsonData)
+                onCompletion(json, error)
+            } else {
+                onCompletion(nil, error)
+            }
+        })
+        task.resume()
+    }
+}
